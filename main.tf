d00fac2c06b8fdc913dfd9657b9ee2d7b96d33ec
Santanu Das <santanu.das@littlehome.co.uk>
2023-04-01 13:20:00 +0000

    KB690 #comment SUBNET2: Updated file content

--- main.tf
+++ main.tf
@@ -1,5 +1,19 @@
 ##
-resource "local_file" "file" {
-  content  = "modb"
-  filename = "${path.module}/modb.txt"
+variable "kb_item" {
+  type        = string
+  description = "Imported from TG"
+}
+
+variable "dependent_value" {
+  type        = string
+  description = "Imported from moda"
+}
+
+resource "local_file" "this" {
+  content  = "Content from file: ${upper(var.dependent_value)}"
+  filename = "${path.module}/${var.kb_item}_modb.txt"
+}
+
+output "file_name" {
+  value = local_file.this.filename
 }
